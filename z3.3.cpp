097c69e690f08af99fc53627fa51d9ad26b95134
Mikołaj Data <miki3867@gmail.com>
2023-10-17 15:04:19 +0200

    3.3 ready

--- z3.3.cpp
+++ z3.3.cpp
@@ -33,6 +33,57 @@ int min(int arr[], int n) {
 
   return pos;
 }
+
+int minPositive(int arr[], int n) {
+  int min = 11;
+  int pos = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < min && arr[i] >= 0) {
+      min = arr[i];
+      pos = i;
+    }
+  }
+
+  return pos;
+}
+
+int minPositiveOdd(int arr[], int n) {
+  int min = 11;
+  int pos = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < min && arr[i] >= 0 && arr[i] % 2 == 0) {
+      min = arr[i];
+      pos = i;
+    }
+  }
+
+  return pos;
+}
+int isEven(int n) { return n % 2 != 0; }
+/*
+ * int nth - Define how many numbers you want to omit
+ * */
+int even(int arr[], int n, int nth) {
+
+  for (int i = 0; i < n; i++) {
+    if (isEven(arr[i])) {
+      if (nth > 0)
+        nth--;
+      else
+        return i;
+    }
+  }
+
+  return -1;
+}
+
+void printArr(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << endl;
+  }
+}
 int main() {
   int *arr, key, i, p;
   arr = new int[LENGHT];
@@ -48,6 +99,19 @@ int main() {
   p = min(arr, LENGHT);
   cout << "Pozycja min: " << p << endl;
 
+  p = minPositive(arr, LENGHT);
+  cout << "Pozycja min dodatnia: " << p << endl;
+
+  p = minPositiveOdd(arr, LENGHT);
+  cout << "Pozycja min dodatnia parzysta: " << p << endl;
+
+  p = even(arr, LENGHT, 1);
+  cout << "Pozycja 1 nieparzysta: " << p << endl;
+
+  p = even(arr, LENGHT, 2);
+  cout << "Pozycja 2 nieparzysta: " << p << endl;
+
+  printArr(arr, LENGHT);
   delete[] arr;
   return 0;
 }
