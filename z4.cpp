96dad4a30e6ce95fa3b51acdf11c94c56986d649
Mikołaj Data <miki3867@gmail.com>
2023-10-17 16:34:21 +0200

    3.4

--- z4.cpp
+++ z4.cpp
@@ -8,15 +8,16 @@ using namespace std;
 
 void printArr(int Z[], bool L[], int n) {
   for (int i = 0; i < n; i++) {
-    cout << i << "." << L[i] << Z[i] << endl;
+    cout << i << "." << L[i] << " Wartosć: " << Z[i] << endl;
   }
 }
 
 int main() {
-  int *Z, key, i, p;
+  int *Z, *P, key, i, p;
   bool *L;
   Z = new int[LENGHT];
   L = new bool[LENGHT];
+  P = new int[LENGHT];
   srand(time(NULL));
 
   for (i = 0; i < LENGHT; i++) {
@@ -26,10 +27,13 @@ int main() {
   key = rand() % 10;
 
   for (i = 0; i < LENGHT; i++) {
-    if (Z[i] == key)
+    if (Z[i] == key) {
       L[i] = true;
+      P[i] = Z[i];
+    }
   }
-  printArr(Z, LENGHT);
+  printArr(Z, L, LENGHT);
+  cout << "Klucz: " << key << endl;
   delete[] Z;
   return 0;
 }
